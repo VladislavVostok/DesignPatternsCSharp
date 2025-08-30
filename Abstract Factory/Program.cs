1d9395d13074c550ad665f93f740cffe688dedf2
Vladislav East <vladislav.sage@yandex.ru>
2025-08-30 19:19:47 +0300

    DP Abstract Factory

---
+++ Abstract Factory/Program.cs
@@ -0,0 +1,160 @@
+﻿/*
+ Абстрактная фабрика - 
+ */
+
+
+
+using System.Globalization;
+
+namespace AbstractFactory.Concept
+{
+
+    // Интерфейс Астрактной Фабрики объявляет набор методов, которые возвращают различные абстрактные продукт.
+    public interface IAbstractFactory
+    {
+        IAbstractProductA CreateProductA();
+        IAbstractProductB CreateProductB();
+    }
+
+
+
+    // Конкретная Фабрика производит семейство продуктов одной вариации.
+    // Фабрика гарантирует совместимость полученных продуктов. 
+    class ConcreteFactory1 : IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA1();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB1();
+        }
+    }
+
+    class ConcreteFactory2 : IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA2();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB2();
+        }
+    }
+
+    // Каждый отдельный продукт семейства продуктов должен иметь базовый интерфейс и все вариации продукта должны реализовывать этот интерфейс.
+    public interface IAbstractProductA
+    {
+        string UsefulFunctionA();
+    }
+
+
+    // Конкретные продукты создаются соответствующими Конкретными фабриками.
+    class ConcreteProductA1 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "Результат продукта A1.";
+
+        }
+    }
+
+    class ConcreteProductA2 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "Результат продукта A2.";
+        }
+    }
+
+    // Базовый интерфейс другого продуктаа.
+    public interface IAbstractProductB
+    {
+
+        // Продукт способен работать самостоятельно
+        string UsefulFunctionB();
+
+        // а также взаимодействовать с Продуктами А тойже вариации.
+        // Абстрактная Фабрика гарантирует, что все продукты, которые она создает, имеют одинаковую вариацию, и совместимы.
+        string AnotherUsefulFunctionB(IAbstractProductA collobarator);
+    }
+
+
+    class ConcreteProductB1 : IAbstractProductB
+    {
+        public string AnotherUsefulFunctionB(IAbstractProductA collobarator)
+        {
+
+            var result  = collobarator.UsefulFunctionA();
+            return $"Результат B1 сотрудничества с {result}";
+        }
+
+        public string UsefulFunctionB()
+        {
+            return "Результат продукта B1.";
+        }
+    }
+
+
+    class ConcreteProductB2 : IAbstractProductB
+    {
+        public string AnotherUsefulFunctionB(IAbstractProductA collobarator)
+        {
+
+            var result = collobarator.UsefulFunctionA();
+            return $"Результат B2 сотрудничества с {result}";
+        }
+
+        public string UsefulFunctionB()
+        {
+            return "Результат продукта B2.";
+        }
+    }
+
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("Клиент: Тестирует клиентский код с фабрикой первого типа..");
+            ClientMethod(new ConcreteFactory1());
+            Console.WriteLine();
+
+
+            Console.WriteLine("Клиент: Тестирует клиентский код с фабрикой второго типа..");
+            ClientMethod(new ConcreteFactory2());
+
+        }
+
+        public void ClientMethod(IAbstractFactory factory)
+        {
+            var productA = factory.CreateProductA();
+            var productB = factory.CreateProductB();
+
+            Console.WriteLine(productB.UsefulFunctionB());
+            Console.WriteLine(productB.AnotherUsefulFunctionB(productA));
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+
+            new Client().Main();
+        }
+    }
+
+
+
+}
---
+++ DBSysteAbstractFactory_Concrete/Program.cs
@@ -0,0 +1,154 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DBSysteAbstractFactory_Concrete
+{
+
+    public interface ICache
+    {
+        void Put(string key, string value);
+        string Get(string key);
+    }
+
+    public class RedisCache : ICache
+    {
+        public string Get(string key)
+        {
+            Console.WriteLine($"Читаем из Redis: {key}");
+            return "данные из Redis";
+        }
+
+        public void Put(string key, string value)
+        {
+            Console.WriteLine($"Сохранение в Redis: {key} = {value}");
+        }
+    }
+
+    public class MemcachedCache : ICache
+    {
+        public string Get(string key)
+        {
+            Console.WriteLine($"Читаем из Memcach: {key}");
+            return "данные из Memcach";
+        }
+
+        public void Put(string key, string value)
+        {
+            Console.WriteLine($"Сохранение в Memcach: {key} = {value}");
+        }
+    }
+
+
+
+
+    public interface IDatabase
+    {
+        void Save(string data);
+        string Load();
+    }
+
+    public class PostgresDatabase : IDatabase
+    {
+        public string Load()
+        {
+            Console.WriteLine($"Читаем из Postgres");
+            return "Данные из Postgres";
+        }
+
+        public void Save(string data)
+        {
+            Console.WriteLine($"Сохранение в Postgres: {data}");
+        }
+    }
+
+    public class MongoDatabase : IDatabase
+    {
+        public string Load()
+        {
+            Console.WriteLine($"Читаем из Mongo");
+            return "Данные из Mongo";
+        }
+
+        public void Save(string data)
+        {
+            Console.WriteLine($"Сохранение в Mongo: {data}");
+        }
+    }
+
+
+    public interface IDataAccessFactory
+    {
+        ICache CreateCache();
+        IDatabase CreateDatabase();
+    }
+
+    public class RedisPostgresFactory : IDataAccessFactory
+    {
+        public ICache CreateCache()
+        {
+            return new RedisCache();
+        }
+
+        public IDatabase CreateDatabase()
+        {
+            return new PostgresDatabase();
+        }
+    }
+
+    public class MemcachedMongoFactory : IDataAccessFactory
+    {
+        public ICache CreateCache()
+        {
+            return new MemcachedCache();
+        }
+
+        public IDatabase CreateDatabase()
+        {
+            return new MongoDatabase();
+        }
+    }
+
+
+    public class Application
+    {
+        private readonly ICache _cache;
+        private readonly IDatabase _database;
+
+        public Application(IDataAccessFactory factory) {
+            _cache = factory.CreateCache(); 
+            _database = factory.CreateDatabase();
+        }
+
+        public void Run()
+        {
+            string data = _database.Load();
+            _cache.Put("key", data);
+            Console.WriteLine("Полученно: " + _cache.Get("key"));
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataAccessFactory factory;
+
+            string env = "Production";
+
+            if(env == "Production")
+            {
+                factory = new MemcachedMongoFactory();
+            }
+            else
+            {
+                factory = new RedisPostgresFactory();
+            }
+
+            var app = new Application(factory);
+            app.Run();
+
+
+            Console.ReadKey();
+        }
+    }
+}

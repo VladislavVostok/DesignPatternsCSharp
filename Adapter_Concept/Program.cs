8619d3101f420e507064e35add6eebafb8fcb618
Vladislav East <vladislav.sage@yandex.ru>
2025-09-14 19:30:29 +0300

    Adapter Decorator

---
+++ Adapter_Concept/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Adapter_Concept
+{
+    public class Program
+    {
+
+        // Целевой класс объявляет интерфейс, с которым может работать клиентский код.
+        public interface ITarget
+        {
+            string GetRequest();
+        }
+
+        // Адаптируемый класс, который будет содержать какое-то полезное поведение,
+        // но его интерфейс не стовместим с существующим кодом. Нуждается в некоторой доработке.
+        class Adaptee
+        {
+            public string GetSpecificRequest() {
+                return "Specific request.";
+            }
+        }
+
+        // Адаптер делает интерфейс Адаптируемого класса совместимым с целевым итерфейсом
+        class UpperAdapter : ITarget
+        {
+            private readonly Adaptee _adaptee;
+
+            public UpperAdapter(Adaptee adaptee) { 
+                _adaptee = adaptee;
+            }
+
+            public string GetRequest()
+            {
+                return $"This is '{this._adaptee.GetSpecificRequest()}'".ToUpper();
+            }
+        }
+
+        class LowerAdapter : ITarget
+        {
+            private readonly Adaptee _adaptee;
+
+            public LowerAdapter(Adaptee adaptee)
+            {
+                _adaptee = adaptee;
+            }
+
+            public string GetRequest()
+            {
+                return $"This is '{this._adaptee.GetSpecificRequest()}'".ToLower();
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget targetUpper = new UpperAdapter(adaptee);
+            Console.WriteLine(targetUpper.GetRequest());
+
+
+            ITarget targetLower = new LowerAdapter(adaptee);
+            Console.WriteLine(targetLower.GetRequest());
+        }
+    }
+}
---
+++ Decorator_Concept/Program.cs
@@ -0,0 +1,132 @@
+﻿namespace Decorator_Concept
+{
+    // Базовый интерфейс Компонента определяет поведение,
+    // которое изменяется декораторами.
+    public abstract class Component
+    {
+        public abstract string Operation();
+    }
+
+    // Конкретные Компоненты предоставляют реализации поведения по умолчанию.
+    // Может быть несколько вариации этих класов.
+    class ConcreteComponent : Component {
+        public override string Operation() {
+            return "ConcreteComponent";
+        }
+    }
+
+    // Базовый класс Декоратора следует тому же интерфейсу, что и другие
+    // компоненты. Основная цель этого класса - определить интерфейс обёртки для 
+    // всех конкретных декораторов.
+    abstract class  Decorator : Component
+    {
+        protected Component _component;
+
+        public Decorator(Component component) { 
+            _component = component;
+        }
+
+        public void SetComponent(Component component) {
+            _component = component;
+        }
+
+
+        // Декоратор делегирует всю работу обёрнутому компоненту.
+        public override string Operation() {
+            if (_component != null)
+            {
+
+                return this._component.Operation();
+            }
+            else {
+
+                return string.Empty;
+            }
+        }
+    }
+
+    // Конкретный Декоратор вызывают обёрнутый объект и изменяют
+    // его результат неторым образом.
+    class ConcreteDecoratorA : Decorator
+    {
+        public ConcreteDecoratorA(Component comp) : base(comp) { }
+
+        public void SetComponent(Component component)
+        {
+            _component = component;
+        }
+
+
+        // Декораторы могут вызывать родительскую реализацию операции,
+        // вместо того, чтобы вызывать обёрнутый объект на прямую
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorA({base.Operation()})";      
+        }
+    }
+
+
+    class ConcreteDecoratorB : Decorator
+    {
+        public ConcreteDecoratorB(Component comp) : base(comp) { }
+
+        public void SetComponent(Component component)
+        {
+            _component = component;
+        }
+
+
+        // Декораторы могут вызывать родительскую реализацию операции,
+        // вместо того, чтобы вызывать обёрнутый объект на прямую
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorB({base.Operation()})";
+        }
+    }
+
+    public class Client
+    {
+        public void ClientCode(Component comp)
+        {
+            Console.WriteLine("Result: " + comp.Operation());
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            bool slack = false;
+            bool face = false;
+            Component stack = new ConcreteComponent();   // Типо емаил
+            string slack_str = string.Empty;
+            string face_str = string.Empty;
+
+            while (true) {
+                
+                if (slack)
+                    stack = new ConcreteDecoratorA(stack);
+
+                if (face)
+                    stack = new ConcreteDecoratorB(stack);
+
+                slack_str = Console.ReadLine();
+                face_str = Console.ReadLine();
+
+                if (slack_str.Trim().ToLower() == "t")
+                    slack = true;
+                else
+                    slack = false;
+
+                if (face_str.Trim().ToLower() == "t")
+                    face = true;
+                else
+                    face = false;
+
+                client.ClientCode(stack);
+            }
+
+        }
+    }
+}
